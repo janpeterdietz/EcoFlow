4aae76d6968e3603a40482b2dff2ff63b4eb4a74
janpeterdietz <jan-peter.dietz@web.de>
2025-04-15 19:20:37 +0200

    Update module.php

--- PowerStream/module.php
+++ PowerStream/module.php
@@ -100,14 +100,18 @@ declare(strict_types=1);
 
 
 			IPS_SetConfiguration($id_Mqtt_Spliiter_Instance, 
-			'{	"ClientID":"' .$ClientID. '",
-				"Password":"' .$PW. '",
-				"Subscriptions":"[{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/quota\",\"QoS\":0},{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/status\",\"QoS\":0}]",
-				"UserName":"' .$UserName. '"}');
+			'{	"ClientID":"' 		.$ClientID. '",
+				"Password":"' 		.$PW. '",
+				"UserName":"' 		.$UserName. '",
+				"Subscriptions":"'  .$Subscriptions. '" 
+			}'
+			);
 				
 			/*
 			'{
-				"ClientID" :'. $ClientID . ',
+				"Subscriptions":"[{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/quota\",\"QoS\":0},{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/status\",\"QoS\":0}]",
+
+			"ClientID" :'. $ClientID . ',
 				"Password" :'. $PW . ',
 				"UserName" :'. $UserName .',
 				"Subscriptions" :'. $Subscriptions . ' 
