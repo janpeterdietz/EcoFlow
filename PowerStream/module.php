803a08917dc11ff54fa82c2417e4b73ea502a443
janpeterdietz <jan-peter.dietz@web.de>
2025-04-15 19:16:04 +0200

    Update module.php

--- PowerStream/module.php
+++ PowerStream/module.php
@@ -100,8 +100,8 @@ declare(strict_types=1);
 
 
 			IPS_SetConfiguration($id_Mqtt_Spliiter_Instance, 
-			'{	"ClientID":" '. $ClientID. '",
-				"Password":"$PW",
+			'{	"ClientID":"' .$ClientID. '",
+				"Password":"' .$PW. '",
 				"Subscriptions":"[{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/quota\",\"QoS\":0},{\"Topic\":\"/open/open-24d53b742e4f42dd874174cbd1bf9717/HW51ZEH49G941031/status\",\"QoS\":0}]",
 				"UserName":"open-24d53b742e4f42dd874174cbd1bf9717"}');
 				
