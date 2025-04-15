3f73fb36fa8da4edacc179dbfca12bdb9cdceead
janpeterdietz <jan-peter.dietz@web.de>
2025-04-15 23:14:00 +0200

    Update module.php

--- PowerStream/module.php
+++ PowerStream/module.php
@@ -181,7 +181,7 @@ declare(strict_types=1);
 
 		public function Send(string $PayLoad)
 		{
-			$this->SendDataToParent(json_encode(['DataID' => '{018EF6B5-AB94-40C6-AA53-46943E824ACF}', "Payload" => $PayLoad]));
+			$this->SendDataToParent(json_encode(['DataID' => '{018EF6B5-AB94-40C6-AA53-46943E824ACF}', "Buffer" => $PayLoad]));
 			//$this->SendDataToParent(json_encode(['DataID' => '{018EF6B5-AB94-40C6-AA53-46943E824ACF}', $PayLoad]));
 		}
 		
