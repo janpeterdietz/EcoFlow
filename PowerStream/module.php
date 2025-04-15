eccaba36a1322ba5812c2f0bb2ef487c04fee7c5
janpeterdietz <jan-peter.dietz@web.de>
2025-04-15 23:26:11 +0200

    Update module.php

--- PowerStream/module.php
+++ PowerStream/module.php
@@ -101,6 +101,17 @@ declare(strict_types=1);
 				"Password":"' 		.$PW. '",
 				"Subscriptions":"[{\"Topic\":\"'.$t1.'\",\"QoS\":0},{\"Topic\":\"'.$t2.'\",\"QoS\":0}]",
 				"UserName":"' 		.$UserName. '"}');
+
+				$subscribe_data = [
+					'DataID'           => '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}',
+					'PacketType'       => 8,
+					'QualityOfService' => 0,
+					'Retain'           => true,
+					'Topic'            => $t1,
+					'Payload'          => ''
+				];
+	
+				$this->Send($subscribe_data);
 				
 		
 
@@ -135,16 +146,7 @@ declare(strict_types=1);
 
 			$this->SetStatus(102); //actice
 
-			$subscribe_data = [
-				'DataID'           => '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}',
-				'PacketType'       => 8,
-				'QualityOfService' => 0,
-				'Retain'           => true,
-				'Topic'            => $t1,
-				'Payload'          => ''
-			];
-
-			$this->Send($subscribe_data);
+		
 
 		}
 
