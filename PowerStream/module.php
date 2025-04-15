b396080523af472c00f259dd7aa7d599bcad5ba7
janpeterdietz <jan-peter.dietz@web.de>
2025-04-15 23:33:51 +0200

    Update module.php

--- PowerStream/module.php
+++ PowerStream/module.php
@@ -103,7 +103,16 @@ declare(strict_types=1);
 				"UserName":"' 		.$UserName. '"}');
 
 
-				
+				$subscribe_data = [
+					'DataID'           => '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}',
+					'PacketType'       => 8,
+					'QualityOfService' => 0,
+					'Retain'           => true,
+					'Topic'            => $t1,
+					'Payload'          => ''
+				];
+	
+				$this->Send($subscribe_data);	
 		
 
 			$result = IPS_ApplyChanges($id_Mqtt_Spliiter_Instance);
@@ -137,16 +146,7 @@ declare(strict_types=1);
 
 			$this->SetStatus(102); //actice
 
-			$subscribe_data = [
-				'DataID'           => '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}',
-				'PacketType'       => 8,
-				'QualityOfService' => 0,
-				'Retain'           => true,
-				'Topic'            => $t1,
-				'Payload'          => ''
-			];
 
-			$this->Send($subscribe_data);
 		
 
 		}
