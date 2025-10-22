5205bf33025fa42479086beb201b91a0a1640d6c
janpeterdietz <jan-peter.dietz@web.de>
2025-10-22 19:23:16 +0200

    Update module.php

--- ECO Flow StreamAC/module.php
+++ ECO Flow StreamAC/module.php
@@ -323,7 +323,8 @@ declare(strict_types=1);
 			}
 
 			$Payload = json_decode($data['Payload'], true);
-			$this->LogMessage('ReceiveData' . print_r($Payload, true), KL_NOTIFY);
+
+			//$this->LogMessage('ReceiveData' . print_r($Payload, true), KL_NOTIFY);
 			
 			if (array_key_exists('gridConnectionPower', $Payload))
 			{
