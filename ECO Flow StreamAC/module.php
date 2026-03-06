b3486581a5973ec791957e0598affcc6d004f713
janpeterdietz <38855481+janpeterdietz@users.noreply.github.com>
2026-03-06 13:36:43 +0100

    Update module.php

--- ECO Flow StreamAC/module.php
+++ ECO Flow StreamAC/module.php
@@ -403,7 +403,7 @@ declare(strict_types=1);
 			{
 				case 1:
 					//$params = ['cfgEnergyStrategyOperateMode' => ['operateTouModeOpen' => true]];
-					$params = ['mppt' =>  ['cfgChgWatts' => 500]];
+					$params = ['acChgCfg' =>  ['chgSpeed' => 500], 'slowChgWatts'=> 400];
 					break;
 				case 2:
 					//$params = ['cfgEnergyStrategyOperateMode' => ['operateScheduledOpen' => true]];
